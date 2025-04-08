3bd8b0834da8b6698a270c7b2abbfec43eaa99ee
Abhishek Singh <abhishek.singh@taazaa.com>
2025-04-08 12:25:52 -0400

    Added Config And Insights

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,15 +6,17 @@ namespace Rex_Azure_App.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            ViewData["Greeting"] = _configuration["Greeting"];
         }
     }
 }
