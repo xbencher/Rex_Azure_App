2df923a30cb690b08d4615bc701a0981959ca821
Abhishek Singh <abhishek.singh@taazaa.com>
2025-04-09 14:28:34 -0400

    Added Connection & migrations

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Rex_Azure_App.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
